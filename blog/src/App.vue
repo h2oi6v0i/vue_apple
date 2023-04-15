b6a62eeb9fdd99fa077b97020a52731bde89467f
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-04-15 15:53:02 +0900

    [2.1-2] : BlogList 컴포넌트 생성

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-4">
+      <h5>Vue 열심히 공부하자</h5>
+      <p>멋지다 전개리~!</p>
+    </div>
+
+    <blog-list :list-data="listData" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogList from './components/BlogList.vue'
+import listData from './assets/blog';
 
 export default {
+  components: { 
+    BlogList 
+  },
+
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      listData: listData,
+    }
   }
 }
 </script>
@@ -21,6 +72,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ blog/src/components/BlogList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <h5>{{ listData[0].title }}</h5>
+            <p>{{ listData[0].date }}</p>
+        </div>
+         <div>
+            <h5>{{ listData[1].title }}</h5>
+            <p>{{ listData[1].date }}</p>
+        </div>
+         <div>
+            <h5>{{ listData[2].title }}</h5>
+            <p>{{ listData[2].date }}</p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'BlogList',
+
+    props: {
+        listData: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
