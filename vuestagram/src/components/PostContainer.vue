be6b49f2c7ece4ecf8e78fa4147a73a9c3b0c4a9
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-04-15 21:26:24 +0900

    [2.1-3] : Ajax

---
+++ vuestagram/src/components/PostContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- ="" 여기에 데이터뿐만 아니라 props 이름도 쓸 수 있음 (props도 일종의 데이터) -->
+    <post-item v-for="(item, index) in postContent" :key="index"
+               :post-content="postContent[index]" 
+    />
+
+  </div>
+</template>
+
+<script>
+import PostItem from './PostItem.vue'
+
+export default {
+  components: { 
+    PostItem 
+  },
+  
+  name: 'PostContainer',
+
+  props: {
+    postContent: Array
+  },
+}
+</script>
+
+<style>
+
+</style>
