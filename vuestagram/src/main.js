f3dca1802b8231e527fcd19d5b2f91644a31c637
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-04-20 23:27:12 +0900

    [3.15-16] : PWA

--- vuestagram/src/main.js
+++ vuestagram/src/main.js
@@ -5,9 +5,12 @@ let emitter = mitt();
 // emitter를 모든 컴포넌트에서 사용할 수 있게 해주는 문법
 let app = createApp(App)
 app.config.globalProperties.emitter = emitter;
+
 // 글로벌한 변수 보관함에 { emitter : emitter } 추가
 
 import store from './store.js'
+
+import './registerServiceWorker'
 
 app.use(store).mount('#app')
 // use(store) : 모든 컴포넌트들이 이 store 안에 있는 데이터들을 공유한다.
